0068d36440bd8cb365ff402df0f9de8b2e8fc27a
Melvin7878 <melvin.turessonnilsson@elev.ga.lbs.se>
2022-10-19 11:59:00 +0200

    Been able to make the progress bar functional

--- Assets/Scripts/ProgressionBarExample.cs
+++ Assets/Scripts/ProgressionBarExample.cs
@@ -1,27 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-// För att använda UI-delar i Unity måste vi inkludera UnityEngine.UI
 using UnityEngine.UI;
 
 public class ProgressionBarExample : MonoBehaviour
 {
-    // Dessa variabler är referenser till RectTransforms (position och storlek för UI element). Just dessa två
-    // motsvarar då bakgrunden för "progression" och den andra är punkten som förflyttar sig över bakgrunden.
-    // SerializeField gör då variablen synlig i Unity Editor.
+    //Variables
+    //Progression bar related variables
     [SerializeField] RectTransform backgroundBar;
     [SerializeField] RectTransform currentProgression;
 
-    // Denna raden gör variablen synlig i editor (SerializeField) och Range säger vilka värden vi godkänner 
-    // Variablen är mellan. I detta läget så kan progressionValue bara vara mellan 0 och 100.
-    [SerializeField][Range(0, 100)] int progressionValue;
+    [SerializeField][Range(0, 100)] float progressionValue;
+
+    //Checkpoint related variables
+    [SerializeField] Transform player;
+    [SerializeField] Transform endPosition;
+
+    private float startD;
+    private float distanceTraveled;
+
+    void Start()
+    {
+        startD = Mathf.Sqrt(Mathf.Pow(endPosition.transform.position.x - player.position.x, 2) + Mathf.Pow(endPosition.transform.position.y - player.position.y, 2));
+    }
 
     void Update()
     {
-        // Omvandla progressionen till 0-1 (att multiplicera med 0 innebär längst till vänster, 1 längst till höger)
-        float progressAsDecimal = progressionValue / 100f;
-        float position = (backgroundBar.rect.width - currentProgression.rect.width ) * progressAsDecimal;
+        float distance = Mathf.Sqrt(Mathf.Pow(endPosition.transform.position.x - player.position.x, 2) + Mathf.Pow(endPosition.transform.position.y - player.position.y, 2));
+        float progressUpdate = 1 - (distance / startD);
+        float position = (backgroundBar.rect.width - currentProgression.rect.width) * progressUpdate;
+
+        //Whole distance
+        progressionValue = progressUpdate;
+
+        //middle distance
+
+
 
         // Denna metoden förflyttar currentProgression (den röda kuben) position är förflyttningen längs progression-baren.
         currentProgression.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, position, currentProgression.rect.width);
