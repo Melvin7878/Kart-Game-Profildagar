2759e4b65054c3f84dfdf540a064548a84b713d6
Melvin7878 <melvin.turessonnilsson@elev.ga.lbs.se>
2022-10-20 15:18:38 +0200

    Have managed to move the math to the game manager adn m any other things
    1. Math moved to game manager
    2. Added player checkpoints which lie in the game manager
    3. We're on good way to fix measuring the whole map with the player checkpoints.
    4. Managed to set up the calculation for the middle point between checkpoints

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,9 +11,27 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     List<Transform> levelCheckpoints;
 
+    [Header("Method related variables")]
+
+    //distance related variables
+    [SerializeField] float result;
+    [SerializeField] float currD;
+    float startD;
+
+    //middle point related variables
+    private float middleZ;
+    private float middleX;
+    private float middleY;
+
+    //common method variables
+    [SerializeField] Transform player;
+    private Transform firstCheckpoint;
+    private Transform secondCheckpoint;
+    private Transform goalCheckpoint;
+
     void Start()
     {
-        if(!playerKart)
+        if (!playerKart)
         {
             playerKart = FindObjectOfType<ArcadeKart>();
         }
@@ -21,6 +39,45 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-        
+        //Set all player checkpoints to transforms
+        for (int i = 0; i < levelCheckpoints.Count; i++)
+        {
+            levelCheckpoints[i].GetComponent<Transform>();
+        }
+
+        //calculate the different differences between the two relevant checkpoints
+        for (int i = 0; i <= levelCheckpoints.Count; i++)
+        {
+            DistanceFormula(player, levelCheckpoints[i], levelCheckpoints[i + 1 % levelCheckpoints.Count]);
+            MiddlePointFormula(levelCheckpoints[i], levelCheckpoints[i + 1 % levelCheckpoints.Count], player);
+            Debug.Log($"This is currD: {currD}\n And this is startD; {startD}");
+        }
+
+        //startD aka the whole distance should be assigned here
+    }
+
+    void DistanceFormula(Transform player, Transform checkpoint1, Transform checkpoint2)
+    {
+        currD = Mathf.Sqrt(Mathf.Pow(checkpoint2.position.x - player.position.x, 2) + Mathf.Pow(checkpoint2.position.y - player.position.y, 2) +
+            Mathf.Pow(checkpoint2.position.z - player.position.z, 2));
+
+        startD = Mathf.Sqrt(Mathf.Pow(checkpoint2.position.x - checkpoint1.position.x, 2) + Mathf.Pow(checkpoint2.position.y - checkpoint1.position.y, 2) +
+           Mathf.Pow(checkpoint2.position.z - checkpoint1.position.z, 2));
+
+        //const float startD = currD;
+        result = currD / startD;
+    }
+
+    void MiddlePointFormula(Transform checkpoint1, Transform checkpoint2, Transform player)
+    {
+        middleX = (checkpoint1.position.x + checkpoint2.position.x) / 2;
+        middleY = (checkpoint1.position.y + checkpoint2.position.y) / 2;
+        middleZ = (checkpoint1.position.z + checkpoint2.position.z) / 2;
+
+        if (player.position.x == middleX || player.position.y == middleY)
+        {
+            //update scoreboard
+            Debug.Log("halfway there");
+        }
     }
 }
--- Assets/Scripts/ProgressionBarExample.cs
+++ Assets/Scripts/ProgressionBarExample.cs
@@ -14,43 +14,41 @@ public class ProgressionBarExample : MonoBehaviour
     
     [SerializeField] float progressionOffset;
 
-    private float startD;
-    private float middleX;
-    private float middleY;
+    public float startD;
+    public float middleX;
+    public float middleY;
 
     [Header ("Checkpoint related variables")]
     [SerializeField] Transform player;
     [SerializeField] Transform endPosition;
     [SerializeField] Transform startPosition;
 
+    private GameObject middleCheck; //put as a prefab later
+
 
     void Start()
     {
-        startD = Mathf.Sqrt(Mathf.Pow(endPosition.position.x - player.position.x, 2) + Mathf.Pow(endPosition.position.y - player.position.y, 2));
-        middleX = Mathf.Abs((startPosition.position.x + endPosition.position.x) / 200);
-        middleY = Mathf.Abs((startPosition.position.y + endPosition.position.y) / 200);   //will be used when we've calculated y position progression
-
-        Debug.Log($"The whole distance from start: {startD}");
-        Debug.Log($"Half of startd: {startD / 2} \n  Half according to the variable: {middleX}");
+        startD = Mathf.Sqrt(Mathf.Pow(endPosition.position.x - startPosition.position.x, 2) + Mathf.Pow(endPosition.position.y - startPosition.position.y, 2));
+        middleX = (startPosition.position.x + endPosition.position.x) / 2;
+        middleY = (startPosition.position.y + endPosition.position.y) / 2;
     }
 
     void Update()
     {
-        //Whole distance value
-        float distance = Mathf.Sqrt(Mathf.Pow(endPosition.position.x - player.position.x, 2) + Mathf.Pow(endPosition.position.y - player.position.y, 2));
-        float position = (backgroundBar.rect.width - currentProgression.rect.width) * progressValue * progressionOffset;
-        progressValue = 1 - (distance / startD);
+        ////Whole distance value
+        //float distance = Mathf.Sqrt(Mathf.Pow(endPosition.position.x - player.position.x, 2) + Mathf.Pow(endPosition.position.y - player.position.y, 2));
+        //float position = (backgroundBar.rect.width - currentProgression.rect.width) * progressValue * progressionOffset;
+        //progressValue = 1 - (distance / startD);
 
 
-        //middle distance value
+        ////middle distance value
+        //if (player.position.x == middleX || player.position.y == middleY)
+        //{
+        //    Debug.Log("yes, halfway there");
+        //}
 
-        if (player.position.x == middleX || player.position.y == middleY)
-        {
-            Debug.Log("yes, halfway there");
-        }
 
-
-        // Denna metoden förflyttar currentProgression (den röda kuben) position är förflyttningen längs progression-baren.
-        currentProgression.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, position, currentProgression.rect.width);
+        //// Denna metoden förflyttar currentProgression (den röda kuben) position är förflyttningen längs progression-baren.
+        //currentProgression.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, position, currentProgression.rect.width);
     }
 }
